7d932a919140342a8131521f7f4feadf083398ed
James Cadman <cadman@roe.ac.uk>
2021-09-28 12:53:20 +0100

    start of a module which will contain functions to reduce lightcurve data from TESS.

---
+++ modules/lightcurve.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
+
+def plot_raw_flux(file):
+    try:
+        data = fits.open(file)
+    except:
+        raise Exception("failed to open fits file")
+    header = data[1].header
+    data = data[1].data
+
+    data = data[~np.isnan(data['PDCSAP_FLUX'])]
+    plt.scatter(data['TIME'], data['PDCSAP_FLUX']/np.mean(data['PDCSAP_FLUX']), s=0.05)
+    plt.xlabel('Time (days)')
+    plt.ylabel('Flux (e-/s)')
+    plt.show()
