39f23f8e05d9481fe53886f39f2ecbe638487add
James Cadman <cadman@roe.ac.uk>
2021-09-28 13:11:29 +0100

    checkpointing current progress with lightcurve.py

--- modules/lightcurve.py
+++ modules/lightcurve.py
@@ -7,6 +7,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
 
+def read_flux_vs_time(file):
+    """
+    Read fits file and return time & flux values
+    """
+    try:
+        data = fits.open(file)
+    except:
+        raise Exception("failed to open fits file")
+    header = data[1].header
+    data = data[1].data
+    return data['TIME'], data['PDCSAP_FLUX']
+
+def phasefold_data(times,period):
+    """
+    Fold the time values on the planet transit period.
+    """
+    return np.mod(times,period)
+
+def remove_trasit_points(time,flux,period=6.51595066707795,duration=2.53343092705777,offset=-2458870.4338364583):
+    """
+    Inputs:
+        period : period of the transiting planet (days).
+        duration : duration of the transit (hours).
+        offset : represents the time since time[0] that the first transit begins, such that transits
+            occur at time[0] + offset + N*period (days).
+    Outputs:
+        time, flux : with points removed which are during a transit.
+    """
+    # first phase fold the data
+    time = phasefold_data(time,period)
+
+    #mask = (time >= time[0] + offset + period) & (time <= time[0] + offset + period + duration)
+    import pdb; pdb.set_trace()
+    return
+
+
 def plot_raw_flux(file):
     try:
         data = fits.open(file)
