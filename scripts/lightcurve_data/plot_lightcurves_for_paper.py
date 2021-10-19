b74693a609cf0ef6e8267337de03c7d75e4ebda4
James Cadman <cadman@roe.ac.uk>
2021-10-19 13:05:01 +0100

    script added to generate TESS lightcurve plots to be included in the paper.

---
+++ scripts/lightcurve_data/plot_lightcurves_for_paper.py
@@ -0,0 +1,22 @@
+"""
+Generate plots of flux vs time for both PDCSAP_FLUX and SAP_FLUX data.
+Fluxes are binned every 30 mins.
+"""
+
+import sys
+import os
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+sys.path.insert(0,os.path.join(os.path.dirname(os.path.realpath(__file__)),'../../modules'))
+import lightcurve as lc
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-file') # path to raw fits file
+parser.add_argument('-plot',action='store_true') # boolean, where to plot results
+args = parser.parse_args()
+file = args.file
+
+for label in ['PDCSAP_FLUX','SAP_FLUX']:
+    lc.plot_binned_flux(file,flux_label=label,save=True)
+lc.plot_phase_folded_flux(file,save=True)
