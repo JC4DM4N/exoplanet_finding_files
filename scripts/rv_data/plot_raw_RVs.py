0361fa9d7e72c68d75406a0b8848a2bfaeb787de
James Cadman <cadman@roe.ac.uk>
2021-10-29 13:24:57 +0100

    updated plotting script to use new functions in radialvelocity.py

--- scripts/rv_data/plot_raw_RVs.py
+++ scripts/rv_data/plot_raw_RVs.py
@@ -1,27 +1,19 @@
 """
 Read the RV file and plot values of RV vs time with associated RV errors.
 """
+import sys
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
+sys.path.insert(0,os.path.join(os.path.dirname(os.path.realpath(__file__)),'../../modules'))
+import radialvelocity as rv
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-file')
 args = parser.parse_args()
 file = args.file
 
-# load the data file with the RVs and their errors
-data = np.genfromtxt(file)
-time = data[:,0]
-rv = data[:,1]
-rv_error = data[:,2]
-
-#plot the RVs vs time
-plt.errorbar(time,rv,yerr=rv_error,fmt='.',c='black',linewidth=1)
-plt.ylabel(r'RV (ms$^{-1}$)',fontsize=15)
-plt.xlabel('BJD - 2450000 (Days)',fontsize=15)
-plt.yticks(fontsize=12)
-plt.xticks(fontsize=12)
-plt.tight_layout()
-plt.savefig('raw_RV_plot.png')
+# load the data file with the RVs and their errors and plot the RVs vs time
+rv.plot_raw_rvs(file, save=True, show=True)
 plt.show()
